932b23450353a4de4a99261aaa1f5e6d7613cea5
iguana3d <igor.cordeiro2016@outlook.com>
2023-02-13 19:17:34 -0300

    fix: adjust css

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ $('.slider-home')
   .slick({
     infinite: true,
     autoplay: true,
+    arrows: false,
     autoplaySpeed: 6000,
     speed: 1000,
     fade: true,
     pauseOnHover: false,
     pauseOnDotsHover: false,
     pauseOnFocus: false,
+    // appendArrows: '.slider-home',
     cssEase: 'linear',
   })
-  .on('beforeChange', function (event, slick, currentSlide) {
+  .on('beforeChange', function () {
     document.getAnimations().forEach((anim) => {
       anim.cancel();
       anim.play();
